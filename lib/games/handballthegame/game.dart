4ec36c32007351eb1ce78469374edf5ec8a57800
Marcus VBP <marcusviniciusbp@gmail.com>
2023-09-06 20:23:59 -0300

    adicionado limite máximo que a camera pode se mover

--- lib/games/handballthegame/game.dart
+++ lib/games/handballthegame/game.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:handballthegame/games/handballthegame/components/court_limits.dart';
 import 'package:handballthegame/games/handballthegame/components/player.dart';
+import 'package:handballthegame/games/handballthegame/components/world_limits.dart';
 import 'package:handballthegame/games/handballthegame/inputs/joystick.dart';
 import 'package:handballthegame/games/handballthegame/players/boy.dart';
 import 'package:handballthegame/games/handballthegame/scenarios/court.dart';
@@ -13,14 +13,15 @@ class HandeballTheGame extends FlameGame
     with DragCallbacks, HasCollisionDetection {
   HandeballTheGame();
   late HandballCourt court;
-  late CourtLimits _limitsCourt;
   late Player _player;
+  late final WorldLimits limits;
 
   final world = World();
   late final CameraComponent cameraComponent;
 
   @override
   FutureOr<void> onLoad() async {
+    limits = WorldLimits();
     await images.loadAll([
       'court.png',
       'post.png',
@@ -39,7 +40,7 @@ class HandeballTheGame extends FlameGame
       position: Vector2(court.center.x - 70, court.center.y - 170),
       character: BoyPlayer(),
     );
-
+    world.add(limits);
     world.add(court);
     world.add(_player);
 
@@ -50,7 +51,29 @@ class HandeballTheGame extends FlameGame
 
   @override
   void update(double dt) {
-    cameraComponent.viewfinder.position = _player.position;
+    cameraComponent.viewfinder.position = _getCameraPosition();
     super.update(dt);
+  }
+
+  Vector2 _getCameraPosition() {
+    double y = _player.position.y;
+    double x = _player.position.x;
+    if (_player.position.y <= -201) {
+      y = -201;
+    } else if (_player.position.y >= 138) {
+      y = 138;
+    } else {
+      y = _player.position.y;
+    }
+
+    if (_player.position.x <= -930) {
+      x = -930;
+    } else if (_player.position.x >= 930) {
+      x = 930;
+    } else {
+      x = _player.position.x;
+    }
+
+    return Vector2(x, y);
   }
 }
